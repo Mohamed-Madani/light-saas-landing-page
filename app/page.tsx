7d7a6f8d26a7f89d6857e9021fe74884893633cc
Mohamed-Madani <m.rabiouhalilou@gmail.com>
2024-09-05 09:16:40 +0000

    Creating Mobile view of the hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Hero } from "@/sections/Hero";
 import { Header } from "../sections/Header";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Hero />
     </div>
   );
 }
--- sections/Header.tsx
+++ sections/Header.tsx
@@ -5,30 +5,44 @@ import MenuIcon from "@/assets/menu.svg";
 
 export const Header = () => {
   return (
-    <header className="sticky top-0">
-    <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
-      <p className="text-white/60 hidden md:block">Streamline your workflow and boost productivity</p>
-      <div className="inline-flex gap-1 items-center">
-        <p>Get started for free</p>
-        <ArrowRight className="w-4 h-4 inline-flex justify-center items-center" />
-      </div>
-    </div>
-    <div className="py-5">
-      <div className="container">
-        <div className="flex items-center justify-between">
-      <Image src={Logo} alt="Saas logo" width={40} height={40} />
-      <MenuIcon className="w-5 h-5 md:hidden" />
-      <nav className="hidden md:flex gap-6 text-black/60 items-center">
-        <a href="#" className="">About</a>
-        <a href="#" className="">Features</a>
-        <a href="#" className="">Customers</a>
-        <a href="#" className="">Updates</a>
-        <a href="#" className="">Help</a>
-        <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex justify-center tracking-tight">Get for free</button>
-      </nav>
+    <header className="sticky top-0 backdrop-blur-sm">
+      <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
+        <p className="text-white/60 hidden md:block">
+          Streamline your workflow and boost productivity
+        </p>
+        <div className="inline-flex gap-1 items-center">
+          <p>Get started for free</p>
+          <ArrowRight className="w-4 h-4 inline-flex justify-center items-center" />
         </div>
       </div>
-    </div>
+      <div className="py-5">
+        <div className="container">
+          <div className="flex items-center justify-between">
+            <Image src={Logo} alt="Saas logo" width={40} height={40} />
+            <MenuIcon className="w-5 h-5 md:hidden" />
+            <nav className="hidden md:flex gap-6 text-black/60 items-center">
+              <a href="#" className="">
+                About
+              </a>
+              <a href="#" className="">
+                Features
+              </a>
+              <a href="#" className="">
+                Customers
+              </a>
+              <a href="#" className="">
+                Updates
+              </a>
+              <a href="#" className="">
+                Help
+              </a>
+              <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex justify-center tracking-tight">
+                Get for free
+              </button>
+            </nav>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
