82dc1fdab964a8ba03157287a2fbd3dc75d45670
Mohamed-Madani <m.rabiouhalilou@gmail.com>
2024-09-07 08:42:52 +0000

    Creating Desktop and mobile view of the logo ticker

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "@/sections/Hero";
-import { Header } from "../sections/Header";
+import { Header } from "@/sections/Header";
+import { LogoTicker } from "@/sections/LogoTicker";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Hero />
+      <LogoTicker/>
     </div>
   );
 }
